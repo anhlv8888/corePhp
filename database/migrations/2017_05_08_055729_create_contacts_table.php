b3db301257eb6c32fb77b1602a09162c67c6c7b4
anhlv268 <anhlv268@gmail.com>
2017-05-08 13:24:24 +0700

    update mac project laravel Ajax

---
+++ database/migrations/2017_05_08_055729_create_contacts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('group_id')->->unsigned();;
+            $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade');;
+            $table->string('name');
+            $table->string('company');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('address');
+            $table->timestamps();
+        });
+
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
