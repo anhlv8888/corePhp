c9a625d686c58668c2bfc7e92a65dabf64353535
anhlv268 <anhlv268@gmail.com>
2017-05-11 11:03:19 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,8 @@
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-    Hello world he he
-|
-*/
+    Hello world he
+|h*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,6 +18,7 @@ Route::group(['prefix'=>'admin'],function (){
     Route::group(['prefix'=>'contact'],function (){
         Route::get('table','ContactController@table')->name('contact.table');
         Route::get('create','ContactController@getCreate');
+        Route::get('update','ContactController@getUpdate');
         Route::post('create','ContactController@postCreate')->name('contact.create');
     });
 });
